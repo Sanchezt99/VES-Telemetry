339284792294cc343ef0710f6e221ac2a4ec469c
Kevin Gutierrez Gomez <kevingtzz@Kevins-MacBook-Air.local>
2020-06-14 07:43:53 -0500

    database get table implemented

--- Sofware/src/app.js
+++ Sofware/src/app.js
@@ -8,6 +8,8 @@ const {
     ipcMain
 } = require('electron');
 const path = require('path');
+const {parse_data} = require('./parser.js');
+const database = require('./database.js');
 
 if (process.env.NODE_ENV !== 'production') require('electron-reload')(__dirname, {
     //electron: path.join(__dirname, '../node_modules', '.bin', 'electron')
@@ -16,7 +18,8 @@ if (process.env.NODE_ENV !== 'production') require('electron-reload')(__dirname,
 let mainWindow;
 let batteryWindow;
 let databaseWindow;
-let data = {};
+
+let local_storage_file_path = null;
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -208,8 +211,10 @@ function create_main_window() {
                     port.pipe(xbeeAPI.parser);
     
                     xbeeAPI.parser.on("data", function(frame) {
-                        parse_data(frame.data);
+                        let data = parse_data(frame.data);
+
                         mainWindow.webContents.send('serial_data', data);
+                        console.log(data);
                         if (batteryWindow) batteryWindow.webContents.send('serial_data', data);
                         mainWindow.webContents.send('serial_connected', true);
                     });
@@ -267,53 +272,16 @@ ipcMain.on('database-click', (event) => {
         create_database_window();
     }
     databaseWindow.webContents.openDevTools();
-})
+});
 
 ipcMain.on('battery-click', (event) => {
     if (batteryWindow == null) {
         create_battery_window();
     }
     //batteryWindow.webContents.openDevTools();
-})
+});
 
 ipcMain.on('local file', (event, path) => {
-    console.log(path);
     event.reply('local file selected', path);
-})
-
-//================================ Serial Parser ============================================//
-
-function parse_data(data_frame) {
-    data.timestamp = parse_time(data_frame[0], data_frame[1], data_frame[2]);
-    data.latitude = parse_coordinate(data_frame[3], data_frame[4], data_frame[5], data_frame[6]);
-    data.longitude = parse_coordinate(data_frame[7], data_frame[8], data_frame[9], data_frame[10]);
-    data.speed = data_frame[11];
-    data.minVolt = parse_twoBytes(data_frame[12], data_frame[13])/1000;
-    data.maxVolt = parse_twoBytes(data_frame[14], data_frame[15])/1000;
-    data.current = parse_twoBytes(data_frame[16], data_frame[17])/10;
-    data.instantVolt = parse_twoBytes(data_frame[18], data_frame[19])/10;
-    data.soc = data_frame[20];
-}
-
-function parse_time(hours, minutes, seconds) {
-    return `${(hours < 10) ? "0" + hours : hours.toString()}`
-            + ':' + `${(minutes < 10) ? "0" + minutes : minutes.toString()}`
-            + ':' + `${(seconds < 10) ? "0" + seconds : seconds.toString()}`;
-}
-
-function parse_coordinate(byte0, byte1, byte2, byte3) {
-    let coordinate;
-    coordinate = 0x000000FF&byte0;
-    coordinate =  (byte1 << 8) | coordinate;
-    coordinate =  (byte2 << 16) | coordinate;
-    coordinate =  (byte3 << 24) | coordinate;
-    return coordinate/Math.pow(10, 6);
-}
-
-function parse_twoBytes(byte0, byte1) {
-    //Big-Endian format
-    let value;
-    value = 0x00FF&byte1;
-    value = (byte0 << 8) | value;
-    return value;
-}
+    local_storage_file_path = path;
+});
--- Sofware/src/views/js/database.js
+++ Sofware/src/views/js/database.js
@@ -1,73 +1,55 @@
 const { ipcRenderer } = require('electron');
-const fs = require('fs');
-const {dialog} = require('electron').remote;
 
-let save_options = {
-    title: "Create Persistence file",
-    buttonLabel: "Create File",
-    filters :[
-        {name: '*', extensions: ['txt']}
-       ]
-};
+const create_btn = document.getElementById('create_btn');
+const start_btn = document.getElementById('start_btn');
+const table_selected_indicator = document.getElementById('table_selected_indicator');
+const table_selected_text = document.getElementById('table_selected_text');
+const table_list = document.getElementById('table_list');
 
-let select_options = {
-    title: "Select Persistence file",
-    buttonLabel: "Select File",
-    filters :[
-        {name: '*', extensions: ['txt']}
-       ]
-};
+var table_is_selected = false;
 
-var local_create_btn = document.getElementById('local_create_btn');
-var local_select_btn = document.getElementById('local_select_btn');
-var local_start_btn = document.getElementById('local_start_btn');
-var local_indicator = document.getElementById('local_state_indicator');
-var local_path_indicator = document.getElementById('local_selection');
+get_tables();
 
-var file_is_selected = false;
+create_btn.addEventListener('click', () => {
 
-local_create_btn.addEventListener('click', () => {
-    dialog.showSaveDialog(save_options).then((res) => {
-        if(res.canceled === true) {
-            alert("File has not been created.");
-            return;
-        }
-        fs.writeFile(res.filePath, "", (err) => {
-            if(err) {
-                console.log('err.message');
-                alert("Error: " + err.message);
-            } else {
-                alert(`File has been created successfully in path: ${res.filePath}`);
-            }
-        });
-    });
 });
 
-local_select_btn.addEventListener('click', () => {
-    dialog.showOpenDialog(select_options).then((res) => {
-        if (res.canceled === true) {
-            alert("File not selected.");
-            return; 
-        }
-
-        ipcRenderer.send('local file', res.filePaths[0]);
-        alert(`File selected: ${res.filePaths[0]}`);
-    });
-});
-
-local_start_btn.addEventListener('click', () => {
+start_btn.addEventListener('click', () => {
     ipcRenderer.send('start storage', true);
 });
 
 ipcRenderer.on('storage state', (event, state) => {
-    if (file_is_selected) {
-        local_start_btn.innerHTML = state ? 'Stop storage' : 'Start storage'; 
-        local_start_btn.className = state ? "btn btn-outline-danger local_btn" : "btn btn-outline-success local_btn";
+    if (table_is_selected) {
+        start_btn.innerHTML = state ? 'Stop storage' : 'Start storage'; 
+        start_btn.className = state ? "btn btn-outline-danger local_btn" : "btn btn-outline-success local_btn";
     }
 });
 
 ipcRenderer.on('local file selected', (event, path) => {
-    local_path_indicator.innerHTML = `File selected: ${path}`;
+    table_selected_text.innerHTML = `File selected: ${path}`;
     local_indicator.style.backgroundColor = 'rgb(86, 209, 82)';
     file_is_selected = true;
 });
+
+ipcRenderer.on('tables update', (event, tables) => {
+    let tbody = document.createElement('tbody');
+    
+    tables.forEach(table => {
+        let trow = document.createElement('tr');
+        let table_name_field = document.createElement('td');
+        let action_field = document.createElement('td');
+        let table_name_field_text = document.createTextNode(table);
+
+        table_name_field.appendChild(table_name_field_text);
+        //add action
+        trow.appendChild(table_name_field);
+        trow.appendChild(action_field);
+        tbody.appendChild(trow);
+    });
+
+    table_list.appendChild(tbody);
+});
+
+function get_tables() {
+    ipcRenderer.send('get tables');
+}
