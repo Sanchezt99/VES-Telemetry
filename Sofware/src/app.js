e3e3485e8b0e4bac80a188e84970eab444042cb1
Kevin Gutierrez Gomez <kevingtzz@Kevins-MacBook-Air.local>
2020-06-19 03:35:51 -0500

    serial class created

--- Sofware/src/app.js
+++ Sofware/src/app.js
@@ -1,53 +1,32 @@
-const SerialPort = require('serialport');
-const xbee_api = require('xbee-api');
-const {
-    app,
-    BrowserWindow,
-    Menu,
-    MenuItem,
-    ipcMain
-} = require('electron');
+const {app, BrowserWindow, Menu, MenuItem, ipcMain} = require('electron');
 const path = require('path');
-const {parse_data} = require('./parser.js');
-const database = require('./database.js');
 
 if (process.env.NODE_ENV !== 'production') require('electron-reload')(__dirname, {
     //electron: path.join(__dirname, '../node_modules', '.bin', 'electron')
 });
 
-let mainWindow;
-let batteryWindow;
-let databaseWindow;
+let mainWindow = null; 
+let batteryWindow = null;
+let databaseWindow = null;
 
-let recording = false;
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(create_main_window);
+app.on('ready', () => {
+    create_main_window();
+    const serial = require('./serial.js'); //This inicialize the serial connection. 
+});
 
 app.on('activate', () => {
-    // En macOS es común volver a crear una ventana en la aplicación cuando el
-    // icono del dock es clicado y no hay otras ventanas abiertas.
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open
     if (mainWindow === null) {
       create_main_window();
     }
-  });
+});
 
-// Quit when all windows are closed.
 app.on('window-all-closed', () => {
 // On macOS it is common for applications and their menu bar
 // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') {
       app.quit()
-    }
-});
-
-app.on('activate', () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) {
-      create_main_window();
     }
 });
 
@@ -217,65 +196,6 @@ function create_battery_window() {
     })
 }
 
-//==================================== Serial connection ===========================//
-
-var xbeeAPI = new xbee_api.XBeeAPI({
-    api_mode: 2
-});
-  
-let connect = setInterval(connect_xbee, 1000);
-
-function connect_xbee() {
-    console.log('Scanning ports...');
-    SerialPort.list().then(ports => {
-        ports.forEach(port => {
-            if (port.manufacturer != undefined && port.manufacturer.includes('FTDI')) { //'FTDI' id the xbee explorer manufacturer
-                port = new SerialPort(port.path, {
-                    baudRate: 115200,
-                    parser: xbeeAPI.rawParser()
-                });
-
-                port.on('error', function(err) {
-                    console.log('Error: ', err.message);
-                });
-
-                port.on('open', () => {
-                    console.log('Serial port opened.');
-                    clearInterval(connect);
-                });
-
-                port.pipe(xbeeAPI.parser);
-
-                xbeeAPI.parser.on("data", function(frame) {
-                    let data = parse_data(frame.data);
-
-                    ipcMain.on('start recording', (event) => {
-                        recording = true;
-                    });
-
-                    ipcMain.on('stop recording', (event) => {
-                        recording = false;
-                    });
-
-                    if (recording) {
-                        database.insert(data);
-                    }
-
-                    if (mainWindow) mainWindow.webContents.send('serial_data', data);
-                    if (mainWindow) mainWindow.webContents.send('serial_connected', true);
-                    if (batteryWindow) batteryWindow.webContents.send('serial_data', data);
-                });
-
-                port.on('close', function() {
-                    console.log('close');
-                    connect = setInterval(connect_xbee, 1000);
-                    return;
-                });
-            } 
-        });
-    });
-}
-
 //=================================== Events =====================================//
 
 ipcMain.on('database-click', (event) => {
@@ -296,3 +216,18 @@ ipcMain.on('local file', (event, path) => {
     event.reply('local file selected', path);
     local_storage_file_path = path;
 });
+
+//================================== Functions ==================================//
+
+function get_mainWindow() {
+    return mainWindow;
+}
+
+function get_batteryWindow() {
+    return batteryWindow;
+}
+
+module.exports = {
+    get_mainWindow,
+    get_batteryWindow
+}
---
+++ Sofware/src/serial.js
@@ -0,0 +1,70 @@
+const app = require('./app.js');
+const {ipcMain} = require('electron');
+const {parse_data} = require('./parser.js');
+const SerialPort = require('serialport');
+const xbee_api = require('xbee-api');
+const database = require('./database.js');
+
+let recording = false;
+
+var xbeeAPI = new xbee_api.XBeeAPI({
+    api_mode: 2
+});
+  
+let connect = setInterval(connect_xbee, 1000);
+
+function connect_xbee() {
+    console.log('Scanning ports...');
+    SerialPort.list().then(ports => {
+        ports.forEach(port => {
+            if (port.manufacturer != undefined && port.manufacturer.includes('FTDI')) { //'FTDI' id the xbee explorer manufacturer
+                port = new SerialPort(port.path, {
+                    baudRate: 115200,
+                    parser: xbeeAPI.rawParser()
+                });
+
+                port.on('error', function(err) {
+                    console.log('Error: ', err.message);
+                });
+
+                port.on('open', () => {
+                    console.log('Serial port opened.');
+                    clearInterval(connect);
+                });
+
+                port.pipe(xbeeAPI.parser);
+
+                xbeeAPI.parser.on("data", function(frame) {
+                    let data = parse_data(frame.data);
+
+                    ipcMain.on('start recording', (event) => {
+                        recording = true;
+                    });
+
+                    ipcMain.on('stop recording', (event) => {
+                        recording = false;
+                    });
+
+                    if (recording) {
+                        database.insert(data);
+                    }
+
+                    let mainWindow = app.get_mainWindow();
+                    let batteryWindow = app.get_batteryWindow();
+                    
+                    if (mainWindow) {
+                        mainWindow.webContents.send('serial_data', data);
+                        mainWindow.webContents.send('serial_connected', true);
+                    }
+                    if (batteryWindow) batteryWindow.webContents.send('serial_data', data);
+                });
+
+                port.on('close', function() {
+                    console.log('close');
+                    connect = setInterval(connect_xbee, 1000);
+                    return;
+                });
+            } 
+        });
+    });
+}
